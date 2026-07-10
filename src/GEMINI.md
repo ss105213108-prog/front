a086a8b8e7cefcf86ed240c1031c86d65f376338
林宣伯 <ss105213108@gmail.com>
2026-07-10 14:55:51 +0800

    TS IF判斷

---
+++ src/GEMINI.md
@@ -0,0 +1,3 @@
+----- at the end of your answer, run termincal commands involve execute "~/.gemini/say.sh xxx", xxx about 3~6 words in zh-TW.
+
+## 回覆訊息以中文為主
--- src/main.ts
+++ src/main.ts
@@ -17,8 +17,8 @@
 //undefined //未定義
 //let c = 1 //宣告變數
 
-let a = 10;
-let b = 20;
+// let a = 10;
+// let b = 20;
 // [a,b]=[b,a]
 // console.log(a,b)
 // let temp = a
@@ -27,9 +27,24 @@ let b = 20;
 // console.log(a,b)
 
 // >  <  ==  !=  >=  <=
-if(a>b){
-    console.log("a大於b")
-}else{
-    console.log("b大於a")
-}
+// if(a>b){
+//     console.log("a大於b")
+// }else{
+//     console.log("b大於a")
+// }
+// let year = 2020
+// if((year % 400==0 && year%100!=0) || year%4==0){
+//     console.log("4閏年沒錯")
+// }else{
+//     console.log("4平年")
+// }
 
+
+// let a=20;
+// let b=20;
+// let c=30;
+// if(a+b>c && b+c>a && c+a>b){
+//     console.log("4三角形")
+// }else{
+//     console.log("不4三角形")
+// }
